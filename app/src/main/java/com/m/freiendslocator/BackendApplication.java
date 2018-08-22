ac5d16c611975b957ed07137bc439250e6523d46
Manish Kumar <search.manish16@gmail.com>
2018-08-22 13:25:48 +0530

    Update BackendApplication.java

--- app/src/main/java/com/m/freiendslocator/BackendApplication.java
+++ app/src/main/java/com/m/freiendslocator/BackendApplication.java
@@ -5,8 +5,8 @@ import android.app.Application;
 import com.backendless.Backendless;
 
 public class BackendApplication extends Application {
-    public  static  final String APPLICATION_ID ="702166DB-6E0B-0D9A-FFEB-04F6052F0600";
-    public static final String API_KEY ="B1BE0748-D85B-2206-FF4B-A6403EBAA400";
+    public  static  final String APPLICATION_ID ="XXXXXXXXXXXX";
+    public static final String API_KEY ="XXXXXXXXXXXXXXXXXXXXXXX";
     public static  final String  SERVER_URL="https://api.backendless.com";
 
     @Override
